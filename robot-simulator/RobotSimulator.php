3f14efba84ae592d8a408c4ee2b6b19629b439b5
rzuckerm <ron.zuckerman@adtran.com>
2025-06-25 18:54:02 -0500

    Update tests for Robot Simulator in PHP

--- robot-simulator/RobotSimulator.php
+++ robot-simulator/RobotSimulator.php
@@ -2,33 +2,42 @@
 
 declare(strict_types=1);
 
-class Robot
+class RobotSimulator
 {
-    public const DIRECTION_NORTH = [0, 1];
-    public const DIRECTION_EAST = [1, 0];
-    public const DIRECTION_SOUTH = [0, -1];
-    public const DIRECTION_WEST = [-1, 0];
     private const INSTRUCTIONS = ["L" => "turnLeft", "R" => "turnRight", "A" => "advance"];
+    private const DIRECTIONS = ["north", "east", "south", "west"];
+    private const VECTORS = ["north" => [0, 1], "east" => [1, 0], "south" => [0, -1], "west" => [-1, 0]];
 
-    public function __construct(public array $position, public array $direction)
+    public function __construct(public array $position, public string $direction)
     {
+    }
+
+    public function getPosition(): array
+    {
+        return $this->position;
+    }
+
+    public function getDirection(): string
+    {
+        return $this->direction;
     }
 
     public function turnRight(): self
     {
-        $this->direction = [$this->direction[1], -$this->direction[0]];
+        $this->direction = self::DIRECTIONS[(array_search($this->direction, self::DIRECTIONS) + 1) % 4];
         return $this;
     }
 
     public function turnLeft(): self
     {
-        $this->direction = [-$this->direction[1], $this->direction[0]];
+        $this->direction = self::DIRECTIONS[(array_search($this->direction, self::DIRECTIONS) + 3) % 4];
         return $this;
     }
 
     public function advance(): self
     {
-        $this->position = [$this->position[0] + $this->direction[0], $this->position[1] + $this->direction[1]];
+        $delta = self::VECTORS[$this->direction];
+        $this->position = [$this->position[0] + $delta[0], $this->position[1] + $delta[1]];
         return $this;
     }
 
