2a04878197cda6e754497d0aff3fab54513f2f90
rzuckerm <ron.zuckerman@adtran.com>
2025-06-30 17:17:32 -0500

    Update tests for Darts in PHP

--- darts/Darts.php
+++ darts/Darts.php
@@ -2,13 +2,9 @@
 
 declare(strict_types=1);
 
-class Darts
-{
-    public int $score = 0;
-    private const SCORES = [10, 10, 5, 5, 5, 5, 1, 1, 1, 1, 1];
+const SCORES = [10, 10, 5, 5, 5, 5, 1, 1, 1, 1, 1];
 
-    public function __construct(float $xAxis, float $yAxis)
-    {
-        $this->score = self::SCORES[(int) ceil(sqrt($xAxis * $xAxis + $yAxis * $yAxis))] ?? 0;
-    }
+function score(float $xAxis, float $yAxis): int
+{
+    return SCORES[(int) ceil(sqrt($xAxis * $xAxis + $yAxis * $yAxis))] ?? 0;
 }
