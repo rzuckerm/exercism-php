f40f52f4435d07fa1d0c13441e23173ea344722f
rzuckerm <rzdulc@gmail.com>
2026-06-28 07:54:14 -0500

    Update Triangle in PHP

--- triangle/Triangle.php
+++ triangle/Triangle.php
@@ -4,19 +4,25 @@ declare(strict_types=1);
 
 class Triangle
 {
+    private bool $isValid;
+
     public function __construct(private int|float $a, private int|float $b, private int|float $c)
     {
-        if (min($a, $b, $c) <= 0 || $a + $b <= $c || $a + $c <= $b || $b + $c <= $a) {
-            throw new Exception("Invalid triangle");
-        }
+        $this->isValid = min($a, $b, $c) > 0 && $a + $b > $c && $a + $c > $b && $b + $c > $a;
     }
 
-    public function kind(): string
+    public function isEquilateral(): bool
     {
-        return match ([$this->a == $this->b, $this->a == $this->c, $this->b == $this->c]) {
-            [true, true, true] => "equilateral",
-            [false, false, false] => "scalene",
-            default => "isosceles",
-        };
+        return $this->isValid && $this->a == $this->b && $this->a == $this->c && $this->b == $this->c;
+    }
+
+    public function isScalene(): bool
+    {
+        return $this->isValid && $this->a != $this->b && $this->a != $this->c && $this->b != $this->c;
+    }
+
+    public function isIsosceles(): bool
+    {
+        return $this->isValid && !$this->isScalene();
     }
 }
